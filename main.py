79b65f747a8289829cf183c3cc1a8e020413466e
NicolasAgustinBiglieri <biglieriagustin@gmail.com>
2024-04-25 21:42:36 -0300

    Tratando de arreglar commits con rebase 6

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ Generamos peticiones desde Thunder Client (como extensión en VSCode)
 """
 
 from fastapi import FastAPI
-from routers import products, users, basic_auth_users, jwt_auth_users
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
 
 app = FastAPI()
 
@@ -32,6 +32,7 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(basic_auth_users.router)
 app.include_router(jwt_auth_users.router)
+app.include_router(users_db.router)
 
 @app.get("/")
 def read_root():
